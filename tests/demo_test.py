8838b112568f5bb1ce508a7526c5ae4ac7b1ed3c
李宝 <libao@vex1023.com>
2016-11-07 00:26:17 +0800

    保存新版本

---
+++ tests/demo_test.py
@@ -0,0 +1,11 @@
+from nose.toolsimport *
+import mymodule
+ 
+def setup():
+    print "SETUP!"
+ 
+def teardown():
+    print "TEAR DOWN!"
+ 
+def test_basic():
+   print "I RAN!"
---
+++ vxAlgo/__init__.py
@@ -0,0 +1,9 @@
+# encoding = utf-8
+'''
+这是一个python项目包的样例
+'''
+
+__version__ = '0.0.1'
+__author__ = 'vex1023'
+__email__ = 'vex1023@qq.com'
+
